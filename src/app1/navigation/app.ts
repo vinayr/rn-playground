af29df6d709d8a4e0a2a3a30dd66a8e606277e2c
vinayr <vinay.rv@gmail.com>
2019-10-01 19:39:11 +0900

    update app1 navigator

--- src/app1/navigation/app.ts
+++ src/app1/navigation/app.ts
@@ -1,36 +1,28 @@
 import { createStackNavigator } from 'react-navigation-stack';
-import { NavigationStackScreenOptions } from 'react-navigation-stack/lib/typescript/types';
 
 import Home from 'app1/screens/home';
 import UserList from 'app1/screens/user/list';
 import UserProfile from 'app1/screens/user/profile';
 
-const navigatorConfig: NavigationStackScreenOptions = {
-  initialRouteName: 'Home',
-  headerLayoutPreset: 'center',
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: '#f4511e',
+const AppNavigator = createStackNavigator(
+  {
+    Home,
+    UserList,
+    UserProfile,
+  },
+  {
+    initialRouteName: 'Home',
+    headerLayoutPreset: 'center',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
     },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
   },
-};
-
-const routeConfig = {
-  Home: {
-    screen: Home,
-  },
-  UserList: {
-    screen: UserList,
-  },
-  UserProfile: {
-    screen: UserProfile,
-  },
-};
-
-const AppNavigator = createStackNavigator(routeConfig, navigatorConfig);
+);
 
 export default AppNavigator;
